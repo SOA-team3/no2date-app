e4eef6a84ed4024f667712daea80ab14fb49e2c1
Brian <brian2000890714@gmail.com>
2024-05-21 21:23:04 +0800

    ruby version modified

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -8,11 +8,27 @@ module No2Date
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/presentation/views'
 
+    # Get all formatted file names in the specified directory
+    def get_filenames(directory, extension)
+      files = Dir.glob(File.join(directory, "*#{extension}"))
+      files.map { |file| File.basename(file) }
+    end
+
+    # Get js directory
+    js_directory = 'app/presentation/assets/js'
+    # Get all filename in JavaScript format
+    js_filenames = get_filenames(js_directory, '.js')
+
+    css_directory = 'app/presentation/assets/css'
+    css_filenames = get_filenames(css_directory, '.css')
+    puts css_filenames
+
     # plugin :assets, css: 'style.css', path: 'app/presentation/assets'
     plugin :assets,
            css: ['nucleo-icons.css', 'nucleo-svg.css', 'perfect-scrollbar.css',
                  'soft-ui-dashboard-tailwind.css', 'soft-ui-dashboard-tailwind.min.css', 'tooltips.css'],
-           js: ['all.js', 'calendar.js', ''],
+          #  js: ['all.js', 'calendar.js', ''],
+           js: js_filenames,
            path: 'app/presentation/assets'
 
     plugin :public, root: 'app/presentation/public'
