1823a383e1f7ef57058ed04b53d4c307e96fc824
Brian <brian2000890714@gmail.com>
2024-06-13 21:18:46 +0800

    appointments_all/events_all.erb structure adjust

---
+++ app/services/get_appointment.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module No2Date
+  # Returns all appointments belonging to an account
+  class GetAppointment
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account, appt_id)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                    .get("#{@config.API_URL}/appointments/#{appt_id}")
+
+      response.code == 200 ? JSON.parse(response.body.to_s)['data'] : nil
+    end
+  end
+end
